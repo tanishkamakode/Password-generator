6a9b81e7a1af9797d94d39e4b5de6bea134a6687
Tanishka <makodetanishka@gmail.com>
2023-02-22 08:49:55 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  // content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      Montserrat: ["Montserrat", "sans-serif"],
+      MontserratAlternates: ["Montserrat Alternates", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
